0ee89e0d3600eac769f46ac3cda8679ca2bda54c
Nikhil <nikhilpulenti@gmail.com>
2023-06-21 13:28:27 -0500

    change in html

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> nikhil .
+          Edit <code>src/App.js</code> koushik.
         </p>
         <a
           className="App-link"
